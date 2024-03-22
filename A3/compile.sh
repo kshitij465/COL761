be466d2e569c17dbd96e7a055707aa830e1cfe36
Darshan Rakhewar <darshan@Darshans-MacBook-Air.local>
2024-03-22 23:54:11 +0530

    compile.sh

--- A3/compile.sh
+++ A3/compile.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
 
-module load apps/anaconda/3
+module load compiler/python/3.9.13/ucs4/gnu/447
+module load compiler/gcc/9.1.0
+
+pip install matplotlib
+pip install scikit-learn
+pip install scipy
+pip install numpy
+pip install falconn
